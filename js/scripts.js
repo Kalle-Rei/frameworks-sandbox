cfafaa655bd2dc644579254b2cc6e5cbe491f221
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-04-05 13:20:47 +0200

    eventListeners and forms

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,20 @@ nameInput.addEventListener('blur', function(event){
     var nameInputValue = event.target.value;
     console.log('value:', nameInputValue);
 
+    var nameInputError = document.getElementById('name-error');
+
     if(nameInputValue == '') return;
 
     if(nameInputValue.length < 2){
         console.log('ERROR! Name should have at least 2 characters')
+        nameInputError.innerText = 'ERROR! Name should have at least 2 characters';
     }
-    
+    else{
+        nameInputError.innerText = '';
+    }
+
+    if (nameInputValue.length > 15) {
+        nameInputError.innerText = 'ERROR! Name should have less than 15 characters';
+    }
+
 });
